98e5ddc2bbe685ee2c7b4e0030841b13e52af5ed
Mann070 <mannprajapati07@gmail.com>
2024-12-24 13:09:33 +0530

    start

---
+++ login.py
@@ -0,0 +1,7 @@
+username = input("Enter the username: ")
+password = int(input("Enter the password: "))
+init_pass = 123456789
+if password == init_pass:
+    print("Welcome")
+else:
+    print("Wrong password!!!")
